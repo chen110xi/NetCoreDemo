5613e8fbd2670502d19e7b68af5fe9d2f8f3236a
Ethan <chen110xi@qq.com>
2017-08-22 18:10:31 +0800

    SQL

---
+++ EfCoreDemo/Test/TestSql.sql
@@ -0,0 +1,14 @@
+﻿drop database [DemoDb]
+
+
+select * from [DemoDb].[dbo].[User];
+select * from [DemoDb].[dbo].[Account];
+
+
+select * from [DemoDb].[dbo].[Class]
+select * from [DemoDb].[dbo].[Students]
+
+
+select * from [DemoDb].[dbo].[Products]
+select * from [DemoDb].[dbo].[Categories]
+select * from [DemoDb].[dbo].[ProductCategories]
