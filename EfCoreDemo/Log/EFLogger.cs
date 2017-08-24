5e8a2a7a437457adf87192aae7398e7b1f1f9e9e
Ethan <chen110xi@qq.com>
2017-08-24 10:12:30 +0800

    添加Log

---
+++ EfCoreDemo/Log/EFLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace EfCoreDemo.Log
+{
+    public class EfLogger : ILogger
+    {
+        private readonly string categoryName;
+
+        public EfLogger(string categoryName) => this.categoryName = categoryName;
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+
+            Debug.WriteLine($"时间:{DateTime.Now.ToString("o")} 日志级别: {logLevel} {eventId.Id} 产生的类{this.categoryName}");
+            //DbCommandLogData data = state as DbCommandLogData;
+            //Debug.WriteLine($"SQL语句:{data.CommandText},\n 执行消耗时间:{data.ElapsedMilliseconds}");
+
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+    }
+}
---
+++ EfCoreDemo/Log/MyFilteredLoggerProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Console;
+
+namespace EfCoreDemo.Log
+{
+    public class MyFilteredLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            // NOTE: 这里要注意,这是 EF Core 1.1的使用方式,如果你用的 EF Core 1.0, 就需把IRelationalCommandBuilderFactory替换成下面的类
+            //       Microsoft.EntityFrameworkCore.Storage.Internal.RelationalCommandBuilderFactory
+            /* */  //1.1.0
+
+            //Microsoft.EntityFrameworkCore.DbLoggerCategory
+
+            if (categoryName == typeof(IRelationalCommandBuilderFactory).FullName)
+            {
+                return new EfLogger(categoryName);
+            }
+            return NullLogger.Instance;
+        }
+        public void Dispose()
+        { }
+    }
+}
